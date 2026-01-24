4aa335ea92d9bbc90ebc761bb26de055b8873408
lfcamarac <lfcamarac@users.noreply.github.com>
2026-01-24 02:49:47 -0400

    fix(website_tecnosoft): Move theme_customizer.js to frontend bundle to resolve editor 502 Bad Gateway

--- website_tecnosoft/__manifest__.py
+++ website_tecnosoft/__manifest__.py
@@ -120,6 +120,7 @@
             'website_tecnosoft/static/src/js/quick_order.js',
             'website_tecnosoft/static/src/js/variant_preview.js',
             'website_tecnosoft/static/src/js/branch_stock.js',
+            'website_tecnosoft/static/src/js/theme_customizer.js',
             'website_tecnosoft/static/src/xml/dynamic_snippets.xml',
             'website_tecnosoft/static/src/xml/ajax_search.xml',
             'website_tecnosoft/static/src/xml/cart_upsell.xml',
@@ -132,7 +133,6 @@
         ],
         'website.assets_wysiwyg': [
             'website_tecnosoft/static/src/js/options.js',
-            'website_tecnosoft/static/src/js/theme_customizer.js',
         ],
     },
     'images': [
