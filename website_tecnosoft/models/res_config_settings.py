323b8f6d5e79d6a490d2e9942bd82c44235a8192
lfcamarac <lfcamarac@users.noreply.github.com>
2026-01-24 12:51:07 -0400

    fix(backend): remove deprecated theme settings (color/font) to fix registry load error

--- website_tecnosoft/models/res_config_settings.py
+++ website_tecnosoft/models/res_config_settings.py
@@ -4,6 +4,4 @@ from odoo import models, fields
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    tecnosoft_primary_color = fields.Char(related='website_id.tecnosoft_primary_color', readonly=False)
-    tecnosoft_secondary_color = fields.Char(related='website_id.tecnosoft_secondary_color', readonly=False)
-    tecnosoft_body_font = fields.Selection(related='website_id.tecnosoft_body_font', readonly=False)
+    tecnosoft_dark_mode_default = fields.Boolean(related='website_id.tecnosoft_dark_mode_default', readonly=False)
